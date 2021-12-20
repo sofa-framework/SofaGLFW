9ffafd18dc0c479cf9c3814ee706a5d23846b1e5
Alex Bilger <alexbilger0@gmail.com>
2021-12-20 08:19:03 +0100

    Use dockspace

--- lib/src/SofaGLFW/SofaGLFWImgui.cpp
+++ lib/src/SofaGLFW/SofaGLFWImgui.cpp
@@ -37,6 +37,7 @@
 
 #if SOFAGLFW_HAS_IMGUI
 #include <imgui.h>
+#include <imgui_internal.h> //imgui_internal.h is included in order to use the DockspaceBuilder API (which is still in development)
 #include <ImGuiFileDialog.h>
 #include <backends/imgui_impl_glfw.h>
 #include <backends/imgui_impl_opengl3.h>
@@ -90,15 +91,55 @@ void imguiDraw(SofaGLFWBaseGUI* baseGUI)
     ImGui_ImplGlfw_NewFrame();
     ImGui::NewFrame();
 
+    ImGuiViewport* viewport = ImGui::GetMainViewport();
+    ImGuiWindowFlags window_flags = ImGuiWindowFlags_MenuBar | ImGuiWindowFlags_NoDocking;
+
+    ImGui::SetNextWindowPos(viewport->Pos);
+    ImGui::SetNextWindowSize(viewport->Size);
+    ImGui::SetNextWindowViewport(viewport->ID);
+    ImGui::PushStyleVar(ImGuiStyleVar_WindowRounding, 0.0f);
+    ImGui::PushStyleVar(ImGuiStyleVar_WindowBorderSize, 0.0f);
+    window_flags |= ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove;
+    window_flags |= ImGuiWindowFlags_NoBringToFrontOnFocus | ImGuiWindowFlags_NoNavFocus;
+    window_flags |= ImGuiWindowFlags_NoBackground;
+
+    ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0.0f, 0.0f));
+    ImGui::Begin("DockSpace", nullptr, window_flags);
+    ImGui::PopStyleVar();
+    ImGui::PopStyleVar(2);
+
+    ImGuiID dockspace_id = ImGui::GetID("MyDockSpace");
+    ImGui::DockSpace(dockspace_id, ImVec2(0.0f, 0.0f), ImGuiDockNodeFlags_PassthruCentralNode | ImGuiDockNodeFlags_NoDockingInCentralNode);
+
+    static auto first_time = true;
+    if (first_time)
+    {
+        first_time = false;
+
+        ImGui::DockBuilderRemoveNode(dockspace_id); // clear any previous layout
+        ImGui::DockBuilderAddNode(dockspace_id, ImGuiDockNodeFlags_PassthruCentralNode | ImGuiDockNodeFlags_NoDockingInCentralNode | ImGuiDockNodeFlags_DockSpace);
+        ImGui::DockBuilderSetNodeSize(dockspace_id, viewport->Size);
+
+        auto dock_id_right = ImGui::DockBuilderSplitNode(dockspace_id, ImGuiDir_Right, 0.4f, nullptr, &dockspace_id);
+        ImGui::DockBuilderDockWindow("Scene Graph", dock_id_right);
+        auto dock_id_down = ImGui::DockBuilderSplitNode(dockspace_id, ImGuiDir_Down, 0.3f, nullptr, &dockspace_id);
+        ImGui::DockBuilderDockWindow("Log", dock_id_down);
+        auto dock_id_left = ImGui::DockBuilderSplitNode(dockspace_id, ImGuiDir_Left, 0.2f, nullptr, &dockspace_id);
+        ImGui::DockBuilderDockWindow("Controls", dock_id_left);
+        ImGui::DockBuilderFinish(dockspace_id);
+    }
+    ImGui::End();
+
+
     const ImGuiIO& io = ImGui::GetIO();
 
     static bool isControlsWindowOpen = true;
     static bool isPerformancesWindowOpen = false;
-    static bool isSceneGraphWindowOpen = false;
+    static bool isSceneGraphWindowOpen = true;
     static bool isDisplayFlagsWindowOpen = false;
     static bool isPluginsWindowOpen = false;
     static bool isComponentsWindowOpen = false;
-    static bool isLogWindowOpen = false;
+    static bool isLogWindowOpen = true;
 
     static bool showFPSInMenuBar = true;
 
