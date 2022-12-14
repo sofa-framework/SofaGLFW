cbb8d123f27aa5601bf1ca8e064c8a3e691463bc
Frederick Roy <froy@lnrobo.com>
2022-12-14 10:40:12 +0900

    fix crash by setting callbacks (called by glfw destructors)

--- SofaGLFW/src/SofaGLFW/SofaGLFWBaseGUI.h
+++ SofaGLFW/src/SofaGLFW/SofaGLFWBaseGUI.h
@@ -94,12 +94,18 @@ public:
     }
     
 private:
+    // GLFW callbacks
     static void error_callback(int error, const char* description);
     static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods);
     static void cursor_position_callback(GLFWwindow* window, double xpos, double ypos);
     static void mouse_button_callback(GLFWwindow* window, int button, int action, int mods);
     static void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
     static void close_callback(GLFWwindow* window);
+    // empty (as in non-implemented) GLFW callbacks
+    static void window_focus_callback(GLFWwindow* window, int focused);
+    static void cursor_enter_callback(GLFWwindow* window, int entered);
+    static void monitor_callback(GLFWmonitor* monitor, int event);
+    static void character_callback(GLFWwindow* window, unsigned int codepoint);
 
     void makeCurrentContext(GLFWwindow* sofaWindow);
     void runStep();
