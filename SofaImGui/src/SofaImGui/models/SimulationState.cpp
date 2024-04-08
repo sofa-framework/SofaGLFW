3e15ac2e08be4319263697231193ed4d71779a50
Eulalie Coevoet <eulalie.coevoet@gmail.com>
2024-04-08 19:12:24 +0200

    [models] forgotten files

---
+++ SofaImGui/src/SofaImGui/models/SimulationState.cpp
@@ -0,0 +1,45 @@
+/******************************************************************************
+ *                 SOFA, Simulation Open-Framework Architecture                *
+ *                    (c) 2006 INRIA, USTL, UJF, CNRS, MGH                     *
+ *                                                                             *
+ * This program is free software; you can redistribute it and/or modify it     *
+ * under the terms of the GNU General Public License as published by the Free  *
+ * Software Foundation; either version 2 of the License, or (at your option)   *
+ * any later version.                                                          *
+ *                                                                             *
+ * This program is distributed in the hope that it will be useful, but WITHOUT *
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or       *
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for    *
+ * more details.                                                               *
+ *                                                                             *
+ * You should have received a copy of the GNU General Public License along     *
+ * with this program. If not, see <http://www.gnu.org/licenses/>.              *
+ *******************************************************************************
+ * Authors: The SOFA Team and external contributors (see Authors.txt)          *
+ *                                                                             *
+ * Contact information: contact@sofa-framework.org                             *
+ ******************************************************************************/
+
+#include <SofaImGui/models/SimulationState.h>
+
+namespace sofaimgui::models {
+
+
+void SimulationState::clearStateData()
+{
+    m_stateData.clear();
+}
+
+void SimulationState::addStateData(StateData &data)
+{
+    m_stateData.push_back(data);
+}
+
+const std::vector<SimulationState::StateData>& SimulationState::getStateData() const
+{
+    return m_stateData;
+}
+
+} // namespace
+
+
---
+++ SofaImGui/src/SofaImGui/models/SimulationState.h
@@ -0,0 +1,55 @@
+/******************************************************************************
+ *                 SOFA, Simulation Open-Framework Architecture                *
+ *                    (c) 2006 INRIA, USTL, UJF, CNRS, MGH                     *
+ *                                                                             *
+ * This program is free software; you can redistribute it and/or modify it     *
+ * under the terms of the GNU General Public License as published by the Free  *
+ * Software Foundation; either version 2 of the License, or (at your option)   *
+ * any later version.                                                          *
+ *                                                                             *
+ * This program is distributed in the hope that it will be useful, but WITHOUT *
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or       *
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for    *
+ * more details.                                                               *
+ *                                                                             *
+ * You should have received a copy of the GNU General Public License along     *
+ * with this program. If not, see <http://www.gnu.org/licenses/>.              *
+ *******************************************************************************
+ * Authors: The SOFA Team and external contributors (see Authors.txt)          *
+ *                                                                             *
+ * Contact information: contact@sofa-framework.org                             *
+ ******************************************************************************/
+#pragma once
+
+#include <sofa/type/Vec.h>
+#include <sofa/defaulttype/RigidTypes.h>
+#include <SofaGLFW/SofaGLFWBaseGUI.h>
+#include <sofa/core/behavior/BaseMechanicalState.h>
+
+namespace sofaimgui::models {
+
+class SimulationState
+{
+   public:
+
+    struct StateData {
+        std::string group;
+        std::string description;
+        sofa::core::BaseData* data;
+    };
+
+    SimulationState() = default;
+    ~SimulationState() = default;
+
+    void clearStateData();
+    void addStateData(StateData &data);
+    const std::vector<StateData>& getStateData() const;
+
+   protected:
+    std::vector<StateData> m_stateData;
+
+};
+
+} // namespace
+
+
