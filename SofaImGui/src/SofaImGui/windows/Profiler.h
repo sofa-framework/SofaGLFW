c928f52cc60e32e0c1269976e549b8575ede511d
lamriaimen <lamrimohamedsaidaimen@gmail.com>
2024-05-22 17:13:18 +0200

    [SofaImGui] Revert changes in profiler

--- SofaImGui/src/SofaImGui/windows/Profiler.h
+++ SofaImGui/src/SofaImGui/windows/Profiler.h
@@ -31,23 +31,22 @@
 #include <imgui.h>
 #include <sofa/simulation/Node.h>
 #include <SimpleIni.h>
-#include "WindowState.h"
 
 
 namespace windows
 {
 
-        /**
-         * @brief Shows the Profiler window.
-         *
-         * This function displays profiling information, including frame durations, timer percentages, and timer durations.
-         *
-         * @param groot The root node of the simulation.
-         * @param windowNameProfiler The name of the Profiler window.
-         * @param isProfilerOpen A reference to a boolean flag indicating if the Profiler window is open.
-         */
-        void showProfiler(sofa::core::sptr<sofa::simulation::Node> groot,
-                          const char* const& windowNameProfiler,
-                          WindowState& winManagerProfiler);
+    /**
+     * @brief Shows the Profiler window.
+     *
+     * This function displays profiling information, including frame durations, timer percentages, and timer durations.
+     *
+     * @param groot The root node of the simulation.
+     * @param windowNameProfiler The name of the Profiler window.
+     * @param isProfilerOpen A reference to a boolean flag indicating if the Profiler window is open.
+     */
+    void showProfiler(sofa::core::sptr<sofa::simulation::Node> groot,
+                      const char* const& windowNameProfiler,
+                      bool& isProfilerOpen);
 
 } // namespace sofaimgui
