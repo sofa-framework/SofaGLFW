sofa-framework/SofaGLFW

commit 2b4655d1c7b0958f425f735d5b41a149956474db

[SofaImGui] list of changes: - Program: we can now set the start position of the program - Display Options: minor esthetic standardization - Scene Graph: adds missing PopStyleColor

SofaImGui/src/SofaImGui/windows/ProgramWindow.cpp
@@ -25,6 +25,11 @@
 #include <SofaImGui/Utils.h>
 #include <SofaImGui/widgets/Buttons.h>
 
+#include <SofaImGui/models/actions/Move.h>
+#include <SofaImGui/models/actions/Pick.h>
+#include <SofaImGui/models/actions/Wait.h>
+#include <SofaImGui/models/modifiers/Repeat.h>
+
 #include <sofa/helper/system/FileSystem.h>
 
 #include <imgui_internal.h>
@@ -80,6 +85,7 @@ void ProgramWindow::showWindow(sofaglfw::SofaGLFWBaseGUI *baseGUI,
             static float zoomCoef = 6.;
             static float minSize = ImGui::GetFrameHeight() * 1.5;
             ProgramSizes().TimelineOneSecondSize = zoomCoef * minSize;
+            ProgramSizes().StartMoveBlockSize = 6. * minSize;
             ImGui::PushStyleColor(ImGuiCol_FrameBg, ImGui::GetColorU32(ImGuiCol_WindowBg));
             if (ImGui::BeginChildFrame(ImGui::GetID(m_name.c_str()), ImVec2(width, height),
                                        ImGuiWindowFlags_AlwaysHorizontalScrollbar))
@@ -248,6 +254,7 @@ void ProgramWindow::showTimeline()
     float indentSize = ImGui::GetFrameHeight();
     ImGui::Indent(indentSize);
 
+    ImGuiWindow* window = ImGui::GetCurrentWindow();
     const ImRect frame_bb(ImVec2(m_trackBeginPos.x, m_trackBeginPos.y - ImGui::GetFrameHeight() * 1.5),
                           ImVec2(m_trackBeginPos.x + width, m_trackBeginPos.y));
     const ImGuiID id = ImGui::GetID("##timeline");
@@ -256,6 +263,7 @@ void ProgramWindow::showTimeline()
     ImGui::SetItemTooltip("Simulation time");
 
     ImGui::BeginGroup(); // Timeline's number (seconds)
+    window->DC.CursorPos.x = m_trackBeginPos.x;
     for (int i=0 ; i<nbSteps; i++)
     {
         std::string text = std::to_string(i) + " s";
@@ -267,12 +275,12 @@ void ProgramWindow::showTimeline()
     }
     ImGui::EndGroup();
 
-    ImGuiWindow* window = ImGui::GetCurrentWindow();
     ImDrawList* drawList = ImGui::GetWindowDrawList();
 
     ImGui::NewLine();
     ImGui::BeginGroup(); // Timeline's lines
     ImGui::SameLine();
+    window->DC.CursorPos.x = m_trackBeginPos.x;
     ImGui::PushStyleVar(ImGuiStyleVar_ItemSpacing, ImVec2(ProgramSizes().TimelineOneSecondSize / 10, 0.f));
     float height = ImGui::GetFrameHeight() / 3.f;
     float heightSpace = height / 4.f;
@@ -364,6 +372,7 @@ int ProgramWindow::showTracks()
 
         ImGui::SameLine();
         m_trackBeginPos = ImGui::GetCurrentWindow()->DC.CursorPos;
+        m_trackBeginPos.x += ProgramSizes().StartMoveBlockSize;
         showBlocks(track, trackIndex);
 
         { // Empty track background
@@ -508,7 +517,17 @@ void ProgramWindow::showBlocks(std::shared_ptr<models::Track> track,
     ImGui::GetCurrentWindow()->DC.CursorPosPrevLine.x = x;
     ImGui::GetCurrentWindow()->DC.CursorPosPrevLine.y = y;
 
-            // Action blocks
+    // StartMove block
+    {
+        std::shared_ptr<models::actions::StartMove> startmove = track->getStartMove();
+        std::string blockLabel = "##StartMove" + std::to_string(trackIndex);
+        if (startmove->getView()->showBlock(blockLabel, ImVec2(ProgramSizes().StartMoveBlockSize, blockHeight)))
+        {
+            track->updateNextMoveInitialPoint(-1, startmove->getWaypoint());
+        }
+    }
+
+    // Action blocks
     const std::vector<std::shared_ptr<models::actions::Action>> &actions = track->getActions();
     sofa::Index actionIndex = 0;
     while(actionIndex < actions.size())

SofaImGui/CMakeLists.txt
@@ -101,6 +101,7 @@ set(HEADER_FILES
     ${SOFAIMGUI_SOURCE_DIR}/models/actions/Action.h
     ${SOFAIMGUI_SOURCE_DIR}/models/actions/Move.h
     ${SOFAIMGUI_SOURCE_DIR}/models/actions/Pick.h
+    ${SOFAIMGUI_SOURCE_DIR}/models/actions/StartMove.h
     ${SOFAIMGUI_SOURCE_DIR}/models/actions/Wait.h
     ${SOFAIMGUI_SOURCE_DIR}/models/modifiers/Modifier.h
     ${SOFAIMGUI_SOURCE_DIR}/models/modifiers/Repeat.h
@@ -150,11 +151,13 @@ set(SOURCE_FILES
 
     ${SOFAIMGUI_SOURCE_DIR}/models/actions/Move.cpp
     ${SOFAIMGUI_SOURCE_DIR}/models/actions/Pick.cpp
+    ${SOFAIMGUI_SOURCE_DIR}/models/actions/StartMove.cpp
     ${SOFAIMGUI_SOURCE_DIR}/models/actions/Wait.cpp
     ${SOFAIMGUI_SOURCE_DIR}/models/modifiers/Repeat.cpp
 
     ${SOFAIMGUI_SOURCE_DIR}/models/actions/views/MoveView.cpp
     ${SOFAIMGUI_SOURCE_DIR}/models/actions/views/PickView.cpp
+    ${SOFAIMGUI_SOURCE_DIR}/models/actions/views/StartMoveView.cpp
     ${SOFAIMGUI_SOURCE_DIR}/models/actions/views/WaitView.cpp
     ${SOFAIMGUI_SOURCE_DIR}/models/modifiers/views/RepeatView.cpp
 

SofaImGui/bindings/src/Binding_ProgramWindow.cpp
@@ -62,7 +62,6 @@ void moduleAddProgramWindow(py::module &m)
             }
         }, "Add pick action to the program."
         );
-
 }
 
 

SofaImGui/resources/ProgramStyle.cpp
@@ -4,9 +4,10 @@ namespace sofaimgui {
 
 ProgramColors::ProgramColors()
 {
-    FrameBg             = ImVec4(1.f, 1.f, 1.f, .3f);
-    MoveBlockBg         = ImVec4(0.39f, 0.57f, 0.6f, 0.5f);
+    FrameBg             = ImVec4(1.00f, 1.00f, 1.00f, 0.3f);
+    MoveBlockBg         = ImVec4(0.39f, 0.57f, 0.60f, 0.5f);
     PickBlockBg         = ImVec4(0.64f, 0.80f, 0.58f, 0.5f);
+    StartMoveBlockBg    = ImVec4(0.60f, 0.60f, 0.60f, 0.5f);
     WaitBlockBg         = ImVec4(0.99f, 0.66f, 0.14f, 0.5f);
     RepeatBlockBg       = ImVec4(0.58f, 0.50f, 0.92f, 0.5f);
     Text                = ImVec4(1.0f, 1.0f, 1.0f, 1.f);

SofaImGui/resources/ProgramStyle.h
@@ -8,6 +8,7 @@ struct ProgramColors
     ImVec4 MoveBlockBg;
     ImVec4 PickBlockBg;
     ImVec4 WaitBlockBg;
+    ImVec4 StartMoveBlockBg;
     ImVec4 RepeatBlockBg;
     ImVec4 Text;
     ImVec4 FrameText;
@@ -23,6 +24,7 @@ struct ProgramSizes
     inline static float InputWidth = 0.;
     inline static float AlignWidth = 0.;
     inline static float TimelineOneSecondSize = 0.;
+    inline static float StartMoveBlockSize = 0.;
 };
 
 }

SofaImGui/src/SofaImGui/ImGuiGUIEngine.h
@@ -89,7 +89,7 @@ public:
     windows::PlottingWindow m_plottingWindow = windows::PlottingWindow("       Plotting", true);
     windows::MyRobotWindow m_myRobotWindow = windows::MyRobotWindow("       My Robot", true);
     windows::MoveWindow m_moveWindow = windows::MoveWindow("       Move", true);
-    windows::DisplayFlagsWindow m_displayFlagsWindow = windows::DisplayFlagsWindow("       Display Flags", false);
+    windows::DisplayFlagsWindow m_displayFlagsWindow = windows::DisplayFlagsWindow("       Display Options", false);
 
 protected:
     std::unique_ptr<sofa::gl::FrameBufferObject> m_fbo;

SofaImGui/src/SofaImGui/models/Program.cpp
@@ -51,9 +51,8 @@ bool Program::importProgram(const std::string &filename)
 
             for(const auto* e = t->FirstChildElement("action"); e != nullptr; e = e->NextSiblingElement("action"))
             {
-                if (strcmp(e->FirstAttribute()->Value(), "move") == 0)
+                if (strcmp(e->FirstAttribute()->Value(), "move") == 0 || strcmp(e->FirstAttribute()->Value(), "startmove") == 0)
                 {
-                    std::shared_ptr<actions::Move> move;
                     if (!e->FindAttribute("wp"))
                         return false;
                     RigidCoord wp;
@@ -71,21 +70,34 @@ bool Program::importProgram(const std::string &filename)
                         return false;
                     bool freeInRotation = e->FindAttribute("freeInRotation")->BoolValue();
 
-                    if (!e->FindAttribute("type"))
-                        return false;
-                    actions::Move::Type type = static_cast<actions::Move::Type>(e->FindAttribute("type")->IntValue());
+                    if (strcmp(e->FirstAttribute()->Value(), "startmove") == 0)
+                    {
+                        auto startmove = track->getStartMove();
+                        startmove->setWaypoint(wp);
+                        startmove->setDuration(duration);
+                        startmove->setFreeInRotation(freeInRotation);
+                        if (e->FindAttribute("comment"))
+                            startmove->setComment(e->Attribute("comment"));
+                    }
+                    else
+                    {
+                        std::shared_ptr<actions::Move> move;
+                        if (!e->FindAttribute("type"))
+                            return false;
+                        actions::Move::Type type = static_cast<actions::Move::Type>(e->FindAttribute("type")->IntValue());
 
-                            // Create the move
-                    move = std::make_shared<actions::Move>(RigidCoord(),
-                                                           wp,
-                                                           duration,
-                                                           m_IPController,
-                                                           freeInRotation,
-                                                           type);
+                        // Create the move
+                        move = std::make_shared<actions::Move>(RigidCoord(),
+                                                               wp,
+                                                               duration,
+                                                               m_IPController,
+                                                               freeInRotation,
+                                                               type);
 
-                    if (e->FindAttribute("comment"))
-                        move->setComment(e->Attribute("comment"));
-                    track->pushMove(move);
+                        if (e->FindAttribute("comment"))
+                            move->setComment(e->Attribute("comment"));
+                        track->pushMove(move);
+                    }
                 }
                 else if (strcmp(e->FirstAttribute()->Value(), "pick") == 0)
                 {
@@ -171,6 +183,28 @@ void Program::exportProgram(const std::string &filename)
         {
             tinyxml2::XMLNode * xmlTrack = document.NewElement("track");
             xmlProgram->InsertEndChild(xmlTrack);
+
+            { // STARTMOVE
+                tinyxml2::XMLElement * xmlStartMove = document.NewElement("action");
+                auto startmove = track->getStartMove();
+                if (xmlStartMove != nullptr)
+                {
+                    xmlStartMove->SetAttribute("name", "startmove");
+                    const auto &waypoint = startmove->getWaypoint();
+                    std::string wp = std::to_string(waypoint[0]) + " "
+                                     + std::to_string(waypoint[1]) + " "
+                                     + std::to_string(waypoint[2]) + " "
+                                     + std::to_string(waypoint[3]) + " "
+                                     + std::to_string(waypoint[4]) + " "
+                                     + std::to_string(waypoint[5]) + " "
+                                     + std::to_string(waypoint[6]) + " ";
+                    xmlStartMove->SetAttribute("wp", wp.c_str());
+                    xmlStartMove->SetAttribute("duration", startmove->getDuration());
+                    xmlStartMove->SetAttribute("freeInRotation", startmove->isFreeInRotation());
+                    xmlStartMove->SetAttribute("comment", startmove->getComment());
+                    xmlTrack->InsertEndChild(xmlStartMove);
+                }
+            }
 
             const auto actions = track->getActions();
             for (const auto& action: actions)

SofaImGui/src/SofaImGui/models/Track.cpp
@@ -25,6 +25,23 @@
 
 namespace sofaimgui::models {
 
+Track::Track(models::IPController::SPtr IPController)
+    : m_IPController(IPController)
+{
+    m_startmove = std::make_shared<models::actions::StartMove>(m_IPController->getTCPTargetInitPosition(),
+                                                               m_IPController->getTCPTargetInitPosition(),
+                                                               0.5,
+                                                               m_IPController,
+                                                               true);
+}
+
+Track::Track(models::IPController::SPtr IPController,
+             std::shared_ptr<actions::StartMove> startMove)
+    : m_IPController(IPController)
+    , m_startmove(startMove)
+{
+}
+
 void Track::clear()
 {
     m_actions.clear();
@@ -69,7 +86,7 @@ void Track::pushAction(std::shared_ptr<actions::Action> action)
 void Track::pushMove(std::shared_ptr<actions::Move> move)
 {
     std::shared_ptr<actions::Move> previous = getPreviousMove(m_actions.size());
-    move->setInitialPoint((previous!=nullptr)? previous->getWaypoint(): m_IPController->getTCPTargetInitPosition());
+    move->setInitialPoint((previous!=nullptr)? previous->getWaypoint(): m_startmove->getWaypoint());
     pushAction(move);
 }
 
@@ -100,7 +117,7 @@ void Track::insertAction(const sofa::Index &actionIndex, std::shared_ptr<actions
 void Track::insertMove(const sofa::Index &actionIndex)
 {
     std::shared_ptr<actions::Move> previous = getPreviousMove(actionIndex);
-    auto move = std::make_shared<actions::Move>((previous!=nullptr)? previous->getWaypoint(): m_IPController->getTCPTargetInitPosition(),
+    auto move = std::make_shared<actions::Move>((previous!=nullptr)? previous->getWaypoint(): m_startmove->getWaypoint(),
                                                 m_IPController->getTCPTargetPosition(),
                                                 actions::Action::DEFAULTDURATION,
                                                 m_IPController,

commit 242b45b0eab9c89dc963a44ece27f09a21fe8f60

[program] modifier blocks: fixes display bug introduced in 2b4655d

SofaImGui/src/SofaImGui/windows/ProgramWindow.cpp
@@ -501,12 +501,23 @@ bool ProgramWindow::showTrackButtons(const int &trackIndex, const char* const me
 void ProgramWindow::showBlocks(std::shared_ptr<models::Track> track,
                                const int& trackIndex)
 {
-    float x = ImGui::GetCurrentWindow()->DC.CursorPos.x ;
-    float y = ImGui::GetCurrentWindow()->DC.CursorPos.y ;
     float blockHeight = ProgramSizes().TrackHeight;
 
+    // StartMove block
+    {
+        std::shared_ptr<models::actions::StartMove> startmove = track->getStartMove();
+        std::string blockLabel = "##StartMove" + std::to_string(trackIndex);
+        if (startmove->getView()->showBlock(blockLabel, ImVec2(ProgramSizes().StartMoveBlockSize, blockHeight)))
+        {
+            track->updateNextMoveInitialPoint(-1, startmove->getWaypoint());
+        }
+    }
+
     // Modifiers blocks
+    float x = ImGui::GetCurrentWindow()->DC.CursorPosPrevLine.x ;
+    float y = ImGui::GetCurrentWindow()->DC.CursorPosPrevLine.y ;
     const std::vector<std::shared_ptr<models::modifiers::Modifier>> &modifiers = track->getModifiers();
+
     sofa::Index modifierIndex = 0;
     while(modifierIndex < modifiers.size())
     {
@@ -537,16 +548,6 @@ void ProgramWindow::showBlocks(std::shared_ptr<models::Track> track,
 
     ImGui::GetCurrentWindow()->DC.CursorPosPrevLine.x = x;
     ImGui::GetCurrentWindow()->DC.CursorPosPrevLine.y = y;
-
-    // StartMove block
-    {
-        std::shared_ptr<models::actions::StartMove> startmove = track->getStartMove();
-        std::string blockLabel = "##StartMove" + std::to_string(trackIndex);
-        if (startmove->getView()->showBlock(blockLabel, ImVec2(ProgramSizes().StartMoveBlockSize, blockHeight)))
-        {
-            track->updateNextMoveInitialPoint(-1, startmove->getWaypoint());
-        }
-    }
 
     // Action blocks
     const std::vector<std::shared_ptr<models::actions::Action>> &actions = track->getActions();