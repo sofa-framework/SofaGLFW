e47c8b5778fe6e732c5c0c292ee64c14814f041e
Paul Baksic <30337881+bakpaul@users.noreply.github.com>
2025-02-12 09:19:36 +0100

    Set priotity to force Imgui as the default GUI (#149)

--- SofaGLFW/src/SofaGLFW/initSofaGLFW.cpp
+++ SofaGLFW/src/SofaGLFW/initSofaGLFW.cpp
@@ -47,7 +47,7 @@ void initExternalModule()
     {
         first = false;
 #if SOFAGLFW_HAVE_SOFA_GUI_COMMON
-        sofa::gui::common::GUIManager::RegisterGUI("glfw", &sofaglfw::SofaGLFWGUI::CreateGUI);
+        sofa::gui::common::GUIManager::RegisterGUI("glfw", &sofaglfw::SofaGLFWGUI::CreateGUI,nullptr,0);
 #endif // SOFAGLFW_HAVE_SOFA_GUI_COMMON
     }
 }
