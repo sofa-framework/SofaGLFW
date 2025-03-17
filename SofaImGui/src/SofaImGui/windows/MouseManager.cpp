43e52f45173b1dda5b90b481fab4fd6a768bf20f
Alex Bilger <alexbilger0@gmail.com>
2025-03-17 10:35:32 +0100

    add some operations

--- SofaImGui/src/SofaImGui/windows/MouseManager.cpp
+++ SofaImGui/src/SofaImGui/windows/MouseManager.cpp
@@ -29,12 +29,7 @@
 namespace windows
 {
 
-struct OperationSettings
-{
-    float stiffness = 1000.0f;
-    float arrowSize = 0.0f;
-    float showFactorSize = 1.0f;
-};
+
 
 void showManagerMouseWindow(const char *const & windowNameMouseManager,
                             WindowState& winManagerMouse,
@@ -42,9 +37,6 @@ void showManagerMouseWindow(const char *const & windowNameMouseManager,
 {
     if(*winManagerMouse.getStatePtr())
     {
-        static OperationSettings settings;
-        static int selectedOperation = 0;
-
         if (auto* pickHandler = baseGUI->getPickHandler())
         {
             ImGui::Begin(windowNameMouseManager, winManagerMouse.getStatePtr());
@@ -78,7 +70,12 @@ void showManagerMouseWindow(const char *const & windowNameMouseManager,
                         for (const auto& [label, creator] : OperationFactory::getInstance()->registry)
                         {
                             const bool isSelected = operation->getId() == label;
-                            ImGui::Selectable(creator->getDescription().c_str(), isSelected);
+                            if (ImGui::Selectable(creator->getDescription().c_str(), isSelected))
+                            {
+                                pickHandler->changeOperation(button, label);
+                            }
+                            if (isSelected)
+                                ImGui::SetItemDefaultFocus();
                         }
                         ImGui::EndCombo();
                     }
@@ -86,21 +83,85 @@ void showManagerMouseWindow(const char *const & windowNameMouseManager,
 
                     ImGui::Separator();
 
-                    if (auto* attachOperation = dynamic_cast<AttachOperation*>(operation))
+                    if (auto* attachOperation = dynamic_cast<AttachOperation*>(operation)) //also valid for ConstraintAttachOperation because of inheritance
                     {
-                        if (ImGui::SliderFloat("Stiffness", &settings.stiffness, 0.0f, 1000.0f))
+                        float stiffness = attachOperation->getStiffness();
+                        if (ImGui::SliderFloat("Stiffness", &stiffness, 0.0f, 1000.0f))
                         {
-                            attachOperation->setStiffness(settings.stiffness);
+                            attachOperation->setStiffness(stiffness);
                         }
 
-                        if (ImGui::SliderFloat("Arrow Size", &settings.arrowSize, 0.0f, 10.0f))
+                        float arrowSize = attachOperation->getArrowSize();
+                        if (ImGui::SliderFloat("Arrow Size", &arrowSize, 0.0f, 10.0f))
                         {
-                            attachOperation->setArrowSize(settings.arrowSize);
+                            attachOperation->setArrowSize(arrowSize);
                         }
 
-                        if (ImGui::SliderFloat("Show Factor Size", &settings.showFactorSize, 1.0f, 5.0f))
+                        float showFactorSize = attachOperation->getShowFactorSize();
+                        if (ImGui::SliderFloat("Show Factor Size", &showFactorSize, 1.0f, 5.0f))
                         {
-                            attachOperation->setShowFactorSize(settings.showFactorSize);
+                            attachOperation->setShowFactorSize(showFactorSize);
+                        }
+                    }
+                    else if (auto* fixOperation = dynamic_cast<FixOperation*>(operation))
+                    {
+                        float stiffness = fixOperation->getStiffness();
+                        if (ImGui::SliderFloat("Stiffness", &stiffness, 0.0f, 100000.0f))
+                        {
+                            attachOperation->setStiffness(stiffness);
+                        }
+                    }
+                    else if (auto* inciseOperation = dynamic_cast<InciseOperation*>(operation))
+                    {
+                        auto incisionMethod = inciseOperation->getIncisionMethod();
+                        if (ImGui::RadioButton("Through segment: Incise from click to click", incisionMethod == 0))
+                        {
+                            inciseOperation->setIncisionMethod(0);
+                        }
+                        if (ImGui::RadioButton("Continually: Incise continually from first click localization", incisionMethod != 0))
+                        {
+                            inciseOperation->setIncisionMethod(1);
+                        }
+
+                        int snapingBorderValue = inciseOperation->getSnapingBorderValue();
+                        if (ImGui::SliderInt("Distance to snap from border (in %)", &snapingBorderValue, 0, 100))
+                        {
+                            inciseOperation->setSnapingBorderValue(snapingBorderValue);
+                        }
+
+                        int snapingValue = inciseOperation->getSnapingValue();
+                        if (ImGui::SliderInt("Distance to snap along path (in %)", &snapingValue, 0, 100))
+                        {
+                            inciseOperation->setSnapingValue(snapingValue);
+                        }
+
+                        bool finishIncision = inciseOperation->getCompleteIncision();
+                        if (ImGui::Checkbox("Complete incision", &finishIncision))
+                        {
+                            inciseOperation->setCompleteIncision(finishIncision);
+                        }
+
+                        bool keepPoint = inciseOperation->getKeepPoint();
+                        if (ImGui::Checkbox("Keep in memory last incision point", &keepPoint))
+                        {
+                            inciseOperation->setKeepPoint(keepPoint);
+                        }
+                    }
+                    else if (auto* topologyOperation = dynamic_cast<TopologyOperation*>(operation))
+                    {
+                    }
+                    else if (auto* sutureOperation = dynamic_cast<AddSutureOperation*>(operation))
+                    {
+                        static float stiffness = 10.0f;
+                        if (ImGui::SliderFloat("Stiffness", &stiffness, 0.0f, 1000.0f))
+                        {
+                            sutureOperation->setStiffness(stiffness);
+                        }
+
+                        static float damping = 1.0f;
+                        if (ImGui::SliderFloat("Damping", &damping, 0.0f, 10.0f))
+                        {
+                            sutureOperation->setDamping(damping);
                         }
                     }
                 }
