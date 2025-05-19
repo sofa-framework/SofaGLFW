777bcafb5bbbbd0774d76b747c9c1e58ea1d2853
Alex Bilger <alxbilger@users.noreply.github.com>
2025-05-19 04:30:46 +0200

    DisplayFlags widget (#188)
    * Introduce DisplayFlags widget
    
    * full integration of DisplayFlags

--- SofaImGui/src/SofaImGui/ImGuiDataWidget.cpp
+++ SofaImGui/src/SofaImGui/ImGuiDataWidget.cpp
@@ -1,24 +1,24 @@
 /******************************************************************************
-*                 SOFA, Simulation Open-Framework Architecture                *
-*                    (c) 2006 INRIA, USTL, UJF, CNRS, MGH                     *
-*                                                                             *
-* This program is free software; you can redistribute it and/or modify it     *
-* under the terms of the GNU General Public License as published by the Free  *
-* Software Foundation; either version 2 of the License, or (at your option)   *
-* any later version.                                                          *
-*                                                                             *
-* This program is distributed in the hope that it will be useful, but WITHOUT *
-* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or       *
-* FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for    *
-* more details.                                                               *
-*                                                                             *
-* You should have received a copy of the GNU General Public License along     *
-* with this program. If not, see <http://www.gnu.org/licenses/>.              *
-*******************************************************************************
-* Authors: The SOFA Team and external contributors (see Authors.txt)          *
-*                                                                             *
-* Contact information: contact@sofa-framework.org                             *
-******************************************************************************/
+ *                 SOFA, Simulation Open-Framework Architecture                *
+ *                    (c) 2006 INRIA, USTL, UJF, CNRS, MGH                     *
+ *                                                                             *
+ * This program is free software; you can redistribute it and/or modify it     *
+ * under the terms of the GNU General Public License as published by the Free  *
+ * Software Foundation; either version 2 of the License, or (at your option)   *
+ * any later version.                                                          *
+ *                                                                             *
+ * This program is distributed in the hope that it will be useful, but WITHOUT *
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or       *
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for    *
+ * more details.                                                               *
+ *                                                                             *
+ * You should have received a copy of the GNU General Public License along     *
+ * with this program. If not, see <http://www.gnu.org/licenses/>.              *
+ *******************************************************************************
+ * Authors: The SOFA Team and external contributors (see Authors.txt)          *
+ *                                                                             *
+ * Contact information: contact@sofa-framework.org                             *
+ ******************************************************************************/
 
 #include <SofaImGui/ImGuiDataWidget.h>
 #include <sofa/core/objectmodel/Base.h>
@@ -27,7 +27,7 @@
 #include <sofa/helper/map.h>
 #include <sofa/helper/OptionsGroup.h>
 #include <sofa/helper/SelectableItem.h>
-
+#include <SofaImGui/widgets/DisplayFlagsWidget.h>
 
 namespace sofaimgui
 {
@@ -647,6 +647,53 @@ void DataWidget<type::RGBAColor>::showWidget(MyData& data)
 }
 
 /***********************************************************************************************************************
+* CompressedRowSparseMatrixConstraint
+**********************************************************************************************************************/
+
+template<typename TBlock, typename TPolicy>
+void showWidgetT(
+Data<linearalgebra::CompressedRowSparseMatrixConstraint<TBlock, TPolicy>>& data)
+{
+    std::stringstream ss;
+    data.getValue().prettyPrint(ss);
+    ImGui::TextWrapped(ss.str().c_str());
+}
+
+template<>
+void DataWidget<linearalgebra::CompressedRowSparseMatrixConstraint<defaulttype::Vec2Types::Deriv>>::showWidget(MyData& data)
+{
+    showWidgetT(data);
+}
+
+template<>
+void DataWidget<linearalgebra::CompressedRowSparseMatrixConstraint<defaulttype::Vec3Types::Deriv>>::showWidget(MyData& data)
+{
+    showWidgetT(data);
+}
+
+template<>
+void DataWidget<linearalgebra::CompressedRowSparseMatrixConstraint<defaulttype::Rigid2Types::Deriv>>::showWidget(MyData& data)
+{
+    showWidgetT(data);
+}
+
+template<>
+void DataWidget<linearalgebra::CompressedRowSparseMatrixConstraint<defaulttype::Rigid3Types::Deriv>>::showWidget(MyData& data)
+{
+    showWidgetT(data);
+}
+
+/***********************************************************************************************************************
+ * DisplayFlags
+ **********************************************************************************************************************/
+
+template<>
+void DataWidget<core::visual::DisplayFlags>::showWidget(MyData& data)
+{
+    showDisplayFlagsWidget(data);
+}
+
+/***********************************************************************************************************************
  * Factory
  **********************************************************************************************************************/
 
@@ -717,4 +764,11 @@ const bool dw_optionsGroup = DataWidgetFactory::Add<helper::OptionsGroup>();
 const bool dw_selectable_items = DataWidgetFactory::Add<helper::BaseSelectableItem>();
 
 const bool dw_rgbacolor = DataWidgetFactory::Add<type::RGBAColor>();
+
+const bool dw_constraintmatrixVec2 = DataWidgetFactory::Add<linearalgebra::CompressedRowSparseMatrixConstraint<defaulttype::Vec2Types::Deriv>>();
+const bool dw_constraintmatrixVec3 = DataWidgetFactory::Add<linearalgebra::CompressedRowSparseMatrixConstraint<defaulttype::Vec3Types::Deriv>>();
+const bool dw_constraintmatrixRigid3 = DataWidgetFactory::Add<linearalgebra::CompressedRowSparseMatrixConstraint<defaulttype::Rigid3Types::Deriv>>();
+const bool dw_constraintmatrixRigid2 = DataWidgetFactory::Add<linearalgebra::CompressedRowSparseMatrixConstraint<defaulttype::Rigid2Types::Deriv>>();
+
+const bool dw_displayFlags = DataWidgetFactory::Add<core::visual::DisplayFlags>();
 }
