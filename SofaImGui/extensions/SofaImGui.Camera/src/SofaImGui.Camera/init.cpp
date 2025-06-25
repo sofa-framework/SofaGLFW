6dc0fcae7830cb68929cbeea77d17a349c8339fc
Alex Bilger <alxbilger@users.noreply.github.com>
2025-06-25 19:08:51 +0200

    Add support of custom GUIs (#204)
    * Initial support for custom additional GUIs
    
    - Introduced AdditionalGUIManager and BaseAdditionalGUI interfaces
    
    - Added AdditionalGUIExample and AdditionalGUIExample2 under an examples folder
    
    - GUIs can now be registered and rendered through a centralized manager
    
    * Expose BaseAdditionalGUI to Python
    
    - Implemented Pybind11 bindings in Binding_AdditionalGUI.{h,cpp}
    
    - Updated CMake and module initialization
    
    * Make AdditionalGUIManager accessible from Python
    
    * clean BaseAdditionalGUI
    
    * fix registration and drawing
    
    * Implement additional GUI in a plugin extension
    
    * add access to the root node
    
    * GUI to control the camera
    
    * remove initial example in SofaImGui
    
    * add description in readme
    
    * revert bindings as it is not yet properly integrated
    
    ---------
    
    Co-authored-by: NolwennDelehonte <nolwenn.delehonte.etu@univ-lille.fr>

---
+++ SofaImGui/extensions/SofaImGui.Camera/src/SofaImGui.Camera/init.cpp
@@ -0,0 +1,72 @@
+/******************************************************************************
+*                 SOFA, Simulation Open-Framework Architecture                *
+*                    (c) 2006 INRIA, USTL, UJF, CNRS, MGH                     *
+*                                                                             *
+* This program is free software; you can redistribute it and/or modify it     *
+* under the terms of the GNU General Public License as published by the Free  *
+* Software Foundation; either version 2 of the License, or (at your option)   *
+* any later version.                                                          *
+*                                                                             *
+* This program is distributed in the hope that it will be useful, but WITHOUT *
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or       *
+* FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for    *
+* more details.                                                               *
+*                                                                             *
+* You should have received a copy of the GNU General Public License along     *
+* with this program. If not, see <http://www.gnu.org/licenses/>.              *
+*******************************************************************************
+* Authors: The SOFA Team and external contributors (see Authors.txt)          *
+*                                                                             *
+* Contact information: contact@sofa-framework.org                             *
+******************************************************************************/
+#include <SofaImGui.Camera/CameraGUI.h>
+
+#include <SofaImGui.Camera/init.h>
+#include <SofaImGui/guis/AdditionalGUIRegistry.h>
+#include <sofa/core/ObjectFactory.h>
+
+namespace sofaimguicamera
+{
+
+void initializePlugin() 
+{
+    static bool first = true;
+    if (first)
+    {
+        first = false;
+
+        const bool registrationSuccessful =
+            sofaimgui::guis::MainAdditionGUIRegistry::registerAdditionalGUI(
+                new CameraGUI());
+    }
+}
+
+}
+
+extern "C" 
+{
+    SOFAIMGUI_CAMERA_API void initExternalModule() 
+    {
+        sofaimguicamera::initializePlugin();
+    }
+
+    SOFAIMGUI_CAMERA_API const char* getModuleName() 
+    {
+        return sofaimguicamera::MODULE_NAME;
+    }
+
+    SOFAIMGUI_CAMERA_API const char* getModuleVersion() 
+    {
+        return sofaimguicamera::MODULE_VERSION;
+    }
+
+    SOFAIMGUI_CAMERA_API const char* getModuleLicense() 
+    {
+        return "LGPL";
+    }
+
+    SOFAIMGUI_CAMERA_API const char* getModuleDescription() 
+    {
+        return "SOFA plugin for SofaImGui.Camera";
+    }
+}
