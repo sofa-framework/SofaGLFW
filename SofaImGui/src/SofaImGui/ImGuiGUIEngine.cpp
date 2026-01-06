978ec7f780dec8dc0d50b133837d5b92ea3a7caa
Lucas Burel <lucasbureltojo@gmail.com>
2026-01-06 16:15:59 +0100

    new feature : select type of snapshot (only JSON)

--- SofaImGui/src/SofaImGui/ImGuiGUIEngine.cpp
+++ SofaImGui/src/SofaImGui/ImGuiGUIEngine.cpp
@@ -420,6 +420,15 @@ void ImGuiGUIEngine::startFrame(sofaglfw::SofaGLFWBaseGUI* baseGUI)
     static bool animate;
     animate = groot->animate_.getValue();
 
+    enum class SaveStep
+    {
+        None,
+        ChooseType,
+        OpenFileDialog
+    };
+
+    static SaveStep saveStep = SaveStep::None;
+
     /***************************************
      * Main menu bar
      **************************************/
@@ -460,6 +469,8 @@ void ImGuiGUIEngine::startFrame(sofaglfw::SofaGLFWBaseGUI* baseGUI)
             ImGui::Separator();
 
             if (ImGui::MenuItem(ICON_FA_FOLDER_CLOSED " Save Snapshot"))
+            {
+                saveStep = SaveStep::ChooseType;
             {
                 // sofa::core::objectmodel::Base* b;
 
@@ -714,6 +725,62 @@ void ImGuiGUIEngine::startFrame(sofaglfw::SofaGLFWBaseGUI* baseGUI)
         baseGUI->setSimulationIsRunning(false);
         sofa::simulation::node::initRoot(baseGUI->getRootNode().get());
         return;
+    }
+
+    if (saveStep == SaveStep::ChooseType)
+    {
+        ImGui::OpenPopup("SnapshotTypeChoice");
+    }
+
+    if ( ImGui::BeginPopupModal("SnapshotTypeChoice", nullptr,
+    ImGuiWindowFlags_AlwaysAutoResize))
+    {
+        ImGui::Text("Choose type of snapshot");
+        ImGui::Separator();
+        if (ImGui::Button("JSON"))
+        {
+            saveStep = SaveStep::OpenFileDialog;
+
+            ImGui::CloseCurrentPopup();
+        }
+        ImGui::SameLine();
+        if (ImGui::Button("nothing"))
+        {
+            ImGui::CloseCurrentPopup();
+        }
+        ImGui::EndPopup();
+    }
+
+    if(saveStep == SaveStep::OpenFileDialog)
+    {
+
+        auto SnapCont = createSnapshot(SnapshotType::JSON);
+        auto visitor = SnapshotVisitor(nullptr,*SnapCont);
+        groot->execute(visitor);
+
+        NFD_Init();
+
+        nfdchar_t* savePath;
+
+        nfdfilteritem_t filterItem[2] = {{"Snapshot code", "json,txt"}, {"Scene file", "py,xml"}};
+
+        nfdresult_t result = NFD_SaveDialog(&savePath, filterItem, 2, NULL, "Untitled.json");
+        if (result == NFD_OKAY) {
+            puts("Save Snapshot success!");
+            puts(savePath);
+
+            std::string path(savePath);
+
+            SnapCont->exportToJSON(path); // Problem : i want to change this (avoid JSON)
+            // remember to free the memory (since NFD_OKAY is returned)
+            NFD_FreePath(savePath);
+        } else {
+            printf("Error: %s\n", NFD_GetError());
+        }
+
+        // Quit NFD
+        NFD_Quit();
+        saveStep = SaveStep::None;
     }
 
     if (m_imguiNeedViewReset)
