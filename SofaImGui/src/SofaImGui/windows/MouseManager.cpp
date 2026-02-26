541c3645c0d21913caa9ca698f423472c1385ef6
Hugo <hugo.talbot@sofa-framework.org>
2026-02-26 10:15:46 +0100

    Fix crashes when changing Mouse operation (#269)
    * Fix crashes when changing Mouse operation
    
    * Apply suggestion from @hugtalbot
    
    * Update SofaImGui/src/SofaImGui/windows/MouseManager.cpp
    
    * Update SofaImGui/src/SofaImGui/windows/MouseManager.cpp
    
    * Fix ID
    
    ---------
    
    Co-authored-by: hugtalbot <hugo.talbot@inria.fr>
    Co-authored-by: Paul Baksic <30337881+bakpaul@users.noreply.github.com>
    Co-authored-by: Paul Baksic <paul.baksic@outlook.fr>

--- SofaImGui/src/SofaImGui/windows/MouseManager.cpp
+++ SofaImGui/src/SofaImGui/windows/MouseManager.cpp
@@ -29,8 +29,6 @@
 namespace windows
 {
 
-
-
 void showManagerMouseWindow(const char *const & windowNameMouseManager,
                             WindowState& winManagerMouse,
                             sofaglfw::SofaGLFWBaseGUI* baseGUI)
@@ -56,6 +54,9 @@ void showManagerMouseWindow(const char *const & windowNameMouseManager,
                 if (ImGui::CollapsingHeader(buttonNames.at(button).c_str()))
                 {
                     auto* operation = pickHandler->getOperation(button);
+                    
+                    if(!operation) continue;
+
                     std::string currentOperationDescription;
 
                     for (const auto& [label, creator] : OperationFactory::getInstance()->registry)
@@ -73,13 +74,15 @@ void showManagerMouseWindow(const char *const & windowNameMouseManager,
                             if (ImGui::Selectable(creator->getDescription().c_str(), isSelected))
                             {
                                 pickHandler->changeOperation(button, label);
+                                operation = pickHandler->getOperation(button); // Re-fetch the operation after call to changeOperation()
+    
+                                if (!operation) continue;
                             }
                             if (isSelected)
                                 ImGui::SetItemDefaultFocus();
                         }
                         ImGui::EndCombo();
                     }
-                    ImGui::PopID();
 
                     ImGui::Separator();
 
@@ -108,7 +111,7 @@ void showManagerMouseWindow(const char *const & windowNameMouseManager,
                         float stiffness = fixOperation->getStiffness();
                         if (ImGui::SliderFloat("Stiffness", &stiffness, 0.0f, 100000.0f))
                         {
-                            attachOperation->setStiffness(stiffness);
+                            fixOperation->setStiffness(stiffness);
                         }
                     }
                     else if (auto* inciseOperation = dynamic_cast<InciseOperation*>(operation))
@@ -195,6 +198,7 @@ void showManagerMouseWindow(const char *const & windowNameMouseManager,
                             sutureOperation->setDamping(damping);
                         }
                     }
+                    ImGui::PopID();
                 }
             }
 
