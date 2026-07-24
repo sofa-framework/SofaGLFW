2619f16c9deb6f6b78878f7eb00272645714b1dd
Alex Bilger <alexbilger0@gmail.com>
2026-07-24 09:18:44 +0200

    move all methods to definition file

--- SofaGLFW/src/SofaGLFW/CaptureSnapshotDrawTool.cpp
+++ SofaGLFW/src/SofaGLFW/CaptureSnapshotDrawTool.cpp
@@ -5,7 +5,55 @@ namespace sofaglfw
 
 CaptureSnapshotDrawTool::CaptureSnapshotDrawTool(std::shared_ptr<SceneSnapshot>& sceneSnapshot)
     : m_sceneSnapshot(sceneSnapshot)
-{}
+{
+}
+void CaptureSnapshotDrawTool::drawPoints(const std::vector<Vec3>& points, float size,
+                                         const RGBAColor& color)
+{
+}
+void CaptureSnapshotDrawTool::drawPoints(const std::vector<Vec3>& points, float size,
+                                         const std::vector<RGBAColor>& color)
+{
+}
+void CaptureSnapshotDrawTool::drawLine(const Vec3& p1, const Vec3& p2, const RGBAColor& color) {}
+void CaptureSnapshotDrawTool::drawInfiniteLine(const Vec3& point, const Vec3& direction,
+                                               const RGBAColor& color, const bool& vanishing)
+{
+}
+void CaptureSnapshotDrawTool::drawInfiniteLine(const Vec3& point, const Vec3& direction,
+                                               const float& size, const RGBAColor& color,
+                                               const bool& vanishing)
+{
+}
+void CaptureSnapshotDrawTool::drawLines(const std::vector<Vec3>& points, float size,
+                                        const RGBAColor& color)
+{
+}
+void CaptureSnapshotDrawTool::drawLines(const std::vector<Vec3>& points, float size,
+                                        const std::vector<RGBAColor>& colors)
+{
+}
+void CaptureSnapshotDrawTool::drawLines(const std::vector<Vec3>& points,
+                                        const std::vector<Vec2i>& index, float size,
+                                        const RGBAColor& color)
+{
+}
+void CaptureSnapshotDrawTool::drawLineStrip(const std::vector<Vec3>& points, float size,
+                                            const RGBAColor& color)
+{
+}
+void CaptureSnapshotDrawTool::drawLineLoop(const std::vector<Vec3>& points, float size,
+                                           const RGBAColor& color)
+{
+}
+void CaptureSnapshotDrawTool::drawDisk(float radius, double from, double to, int resolution,
+                                       const RGBAColor& color)
+{
+}
+void CaptureSnapshotDrawTool::drawCircle(float radius, float lineThickness, int resolution,
+                                         const RGBAColor& color)
+{
+}
 
 void CaptureSnapshotDrawTool::drawTriangles(const std::vector<Vec3>& points, const RGBAColor& color)
 {
@@ -14,5 +62,180 @@ void CaptureSnapshotDrawTool::drawTriangles(const std::vector<Vec3>& points, con
         {
             drawTool->drawTriangles(pointsCopy, colorCopy);
         });
+}
+void CaptureSnapshotDrawTool::drawTriangles(const std::vector<Vec3>& points, const Vec3& normal,
+                                            const RGBAColor& color)
+{
+}
+void CaptureSnapshotDrawTool::drawTriangles(const std::vector<Vec3>& points,
+                                            const std::vector<Vec3i>& index,
+                                            const std::vector<Vec3>& normal, const RGBAColor& color)
+{
+}
+void CaptureSnapshotDrawTool::drawTriangles(const std::vector<Vec3>& points,
+                                            const std::vector<Vec3i>& index,
+                                            const std::vector<Vec3>& normal,
+                                            const std::vector<RGBAColor>& color)
+{
+}
+void CaptureSnapshotDrawTool::drawTriangles(const std::vector<Vec3>& points,
+                                            const std::vector<RGBAColor>& color)
+{
+}
+void CaptureSnapshotDrawTool::drawTriangles(const std::vector<Vec3>& points,
+                                            const std::vector<Vec3>& normal,
+                                            const std::vector<RGBAColor>& color)
+{
+}
+void CaptureSnapshotDrawTool::drawTriangleStrip(const std::vector<Vec3>& points,
+                                                const std::vector<Vec3>& normal,
+                                                const RGBAColor& color)
+{
+}
+void CaptureSnapshotDrawTool::drawTriangleFan(const std::vector<Vec3>& points,
+                                              const std::vector<Vec3>& normal,
+                                              const RGBAColor& color)
+{
+}
+void CaptureSnapshotDrawTool::drawFrame(const Vec3& position, const Quaternion& orientation,
+                                        const Vec3f& size)
+{
+}
+void CaptureSnapshotDrawTool::drawSpheres(const std::vector<Vec3>& points,
+                                          const std::vector<float>& radius, const RGBAColor& color)
+{
+}
+void CaptureSnapshotDrawTool::drawSpheres(const std::vector<Vec3>& points, float radius,
+                                          const RGBAColor& color)
+{
+}
+void CaptureSnapshotDrawTool::drawFakeSpheres(const std::vector<Vec3>& points,
+                                              const std::vector<float>& radius,
+                                              const RGBAColor& color)
+{
+}
+void CaptureSnapshotDrawTool::drawFakeSpheres(const std::vector<Vec3>& points, float radius,
+                                              const RGBAColor& color)
+{
+}
+void CaptureSnapshotDrawTool::drawCone(const Vec3& p1, const Vec3& p2, float radius1, float radius2,
+                                       const RGBAColor& color, int subd)
+{
+}
+void CaptureSnapshotDrawTool::drawCube(const float& radius, const RGBAColor& color, const int& subd)
+{
+}
+void CaptureSnapshotDrawTool::drawCylinder(const Vec3& p1, const Vec3& p2, float radius,
+                                           const RGBAColor& color, int subd)
+{
+}
+void CaptureSnapshotDrawTool::drawCapsule(const Vec3& p1, const Vec3& p2, float radius,
+                                          const RGBAColor& color, int subd)
+{
+}
+void CaptureSnapshotDrawTool::drawArrow(const Vec3& p1, const Vec3& p2, float radius,
+                                        const RGBAColor& color, int subd)
+{
+}
+void CaptureSnapshotDrawTool::drawArrow(const Vec3& p1, const Vec3& p2, float radius,
+                                        float coneLength, const RGBAColor& color, int subd)
+{
+}
+void CaptureSnapshotDrawTool::drawArrow(const Vec3& p1, const Vec3& p2, float radius,
+                                        float coneLength, float coneRadius, const RGBAColor& color,
+                                        int subd)
+{
+}
+void CaptureSnapshotDrawTool::drawCross(const Vec3& p, float length, const RGBAColor& color) {}
+void CaptureSnapshotDrawTool::drawPlus(const float& radius, const RGBAColor& color, const int& subd)
+{
+}
+void CaptureSnapshotDrawTool::drawPoint(const Vec3& p, const RGBAColor& c) {}
+void CaptureSnapshotDrawTool::drawPoint(const Vec3& p, const Vec3& n, const RGBAColor& c) {}
+void CaptureSnapshotDrawTool::drawTriangle(const Vec3& p1, const Vec3& p2, const Vec3& p3,
+                                           const Vec3& normal)
+{
+}
+void CaptureSnapshotDrawTool::drawTriangle(const Vec3& p1, const Vec3& p2, const Vec3& p3,
+                                           const Vec3& normal, const RGBAColor& c)
+{
+}
+void CaptureSnapshotDrawTool::drawTriangle(const Vec3& p1, const Vec3& p2, const Vec3& p3,
+                                           const Vec3& normal, const RGBAColor& c1,
+                                           const RGBAColor& c2, const RGBAColor& c3)
+{
+}
+void CaptureSnapshotDrawTool::drawTriangle(const Vec3& p1, const Vec3& p2, const Vec3& p3,
+                                           const Vec3& normal1, const Vec3& normal2,
+                                           const Vec3& normal3, const RGBAColor& c1,
+                                           const RGBAColor& c2, const RGBAColor& c3)
+{
+}
+void CaptureSnapshotDrawTool::drawQuad(const Vec3& p1, const Vec3& p2, const Vec3& p3,
+                                       const Vec3& p4, const Vec3& normal)
+{
+}
+void CaptureSnapshotDrawTool::drawQuad(const Vec3& p1, const Vec3& p2, const Vec3& p3,
+                                       const Vec3& p4, const Vec3& normal, const RGBAColor& c)
+{
+}
+void CaptureSnapshotDrawTool::drawQuad(const Vec3& p1, const Vec3& p2, const Vec3& p3,
+                                       const Vec3& p4, const Vec3& normal, const RGBAColor& c1,
+                                       const RGBAColor& c2, const RGBAColor& c3,
+                                       const RGBAColor& c4)
+{
+}
+void CaptureSnapshotDrawTool::drawQuad(const Vec3& p1, const Vec3& p2, const Vec3& p3,
+                                       const Vec3& p4, const Vec3& normal1, const Vec3& normal2,
+                                       const Vec3& normal3, const Vec3& normal4,
+                                       const RGBAColor& c1, const RGBAColor& c2,
+                                       const RGBAColor& c3, const RGBAColor& c4)
+{
+}
+void CaptureSnapshotDrawTool::drawQuads(const std::vector<Vec3>& points, const RGBAColor& color) {}
+void CaptureSnapshotDrawTool::drawQuads(const std::vector<Vec3>& points,
+                                        const std::vector<RGBAColor>& colors)
+{
+}
+void CaptureSnapshotDrawTool::drawTetrahedron(const Vec3& p0, const Vec3& p1, const Vec3& p2,
+                                              const Vec3& p3, const RGBAColor& color)
+{
+}
+void CaptureSnapshotDrawTool::drawScaledTetrahedron(const Vec3& p0, const Vec3& p1, const Vec3& p2,
+                                                    const Vec3& p3, const RGBAColor& color,
+                                                    const float scale)
+{
+}
+void CaptureSnapshotDrawTool::drawTetrahedra(const std::vector<Vec3>& points,
+                                             const RGBAColor& color)
+{
+}
+void CaptureSnapshotDrawTool::drawScaledTetrahedra(const std::vector<Vec3>& points,
+                                                   const RGBAColor& color, const float scale)
+{
+}
+void CaptureSnapshotDrawTool::drawHexahedron(const Vec3& p0, const Vec3& p1, const Vec3& p2,
+                                             const Vec3& p3, const Vec3& p4, const Vec3& p5,
+                                             const Vec3& p6, const Vec3& p7, const RGBAColor& color)
+{
+}
+void CaptureSnapshotDrawTool::drawHexahedra(const std::vector<Vec3>& points, const RGBAColor& color)
+{
+}
+void CaptureSnapshotDrawTool::drawScaledHexahedra(const std::vector<Vec3>& points,
+                                                  const RGBAColor& color, const float scale)
+{
+}
+void CaptureSnapshotDrawTool::drawSphere(const Vec3& p, float radius) {}
+void CaptureSnapshotDrawTool::drawSphere(const Vec3& p, float radius, const RGBAColor& color) {}
+void CaptureSnapshotDrawTool::drawEllipsoid(const Vec3& p, const Vec3& radii) {}
+void CaptureSnapshotDrawTool::drawBoundingBox(const Vec3& min, const Vec3& max, float size) {}
+void CaptureSnapshotDrawTool::draw3DText(const Vec3& p, float scale, const RGBAColor& color,
+                                         const char* text)
+{
+}
+void CaptureSnapshotDrawTool::draw3DText_Indices(const std::vector<Vec3>& positions, float scale,
+                                                 const RGBAColor& color)
+{
 }
 }  // namespace sofaglfw
--- SofaGLFW/src/SofaGLFW/CaptureSnapshotDrawTool.h
+++ SofaGLFW/src/SofaGLFW/CaptureSnapshotDrawTool.h
@@ -14,111 +14,111 @@ public:
     explicit CaptureSnapshotDrawTool(std::shared_ptr<SceneSnapshot>& sceneSnapshot);
 
     void init() override{}
-    void drawPoints(const std::vector<Vec3>& points, float size, const RGBAColor& color) override{}
+    void drawPoints(const std::vector<Vec3>& points, float size, const RGBAColor& color) override;
     void drawPoints(const std::vector<Vec3>& points, float size,
-                    const std::vector<RGBAColor>& color) override{}
-    void drawLine(const Vec3& p1, const Vec3& p2, const RGBAColor& color) override{}
+                    const std::vector<RGBAColor>& color) override;
+    void drawLine(const Vec3& p1, const Vec3& p2, const RGBAColor& color) override;
     void drawInfiniteLine(const Vec3& point, const Vec3& direction, const RGBAColor& color,
-                          const bool& vanishing) override{}
+                          const bool& vanishing) override;
     void drawInfiniteLine(const Vec3& point, const Vec3& direction, const float& size,
-                          const RGBAColor& color, const bool& vanishing) override{}
-    void drawLines(const std::vector<Vec3>& points, float size, const RGBAColor& color) override{}
+                          const RGBAColor& color, const bool& vanishing) override;
+    void drawLines(const std::vector<Vec3>& points, float size, const RGBAColor& color) override;
     void drawLines(const std::vector<Vec3>& points, float size,
-                   const std::vector<RGBAColor>& colors) override{}
+                   const std::vector<RGBAColor>& colors) override;
     void drawLines(const std::vector<Vec3>& points, const std::vector<Vec2i>& index, float size,
-                   const RGBAColor& color) override{}
+                   const RGBAColor& color) override;
     void drawLineStrip(const std::vector<Vec3>& points, float size,
-                       const RGBAColor& color) override{}
-    void drawLineLoop(const std::vector<Vec3>& points, float size, const RGBAColor& color) override{}
+                       const RGBAColor& color) override;
+    void drawLineLoop(const std::vector<Vec3>& points, float size, const RGBAColor& color) override;
     void drawDisk(float radius, double from, double to, int resolution,
-                  const RGBAColor& color) override{}
+                  const RGBAColor& color) override;
     void drawCircle(float radius, float lineThickness, int resolution,
-                    const RGBAColor& color) override{}
-    void drawTriangles(const std::vector<Vec3>& points, const RGBAColor& color) override;
+                    const RGBAColor& color) override;
+    auto drawTriangles(const std::vector<Vec3>& points, const RGBAColor& color) -> void override;
     void drawTriangles(const std::vector<Vec3>& points, const Vec3& normal,
-                       const RGBAColor& color) override{}
+                       const RGBAColor& color) override;
     void drawTriangles(const std::vector<Vec3>& points, const std::vector<Vec3i>& index,
-                       const std::vector<Vec3>& normal, const RGBAColor& color) override{}
+                       const std::vector<Vec3>& normal, const RGBAColor& color) override;
     void drawTriangles(const std::vector<Vec3>& points, const std::vector<Vec3i>& index,
                        const std::vector<Vec3>& normal,
-                       const std::vector<RGBAColor>& color) override{}
+                       const std::vector<RGBAColor>& color) override;
     void drawTriangles(const std::vector<Vec3>& points,
-                       const std::vector<RGBAColor>& color) override{}
+                       const std::vector<RGBAColor>& color) override;
     void drawTriangles(const std::vector<Vec3>& points, const std::vector<Vec3>& normal,
-                       const std::vector<RGBAColor>& color) override{}
+                       const std::vector<RGBAColor>& color) override;
     void drawTriangleStrip(const std::vector<Vec3>& points, const std::vector<Vec3>& normal,
-                           const RGBAColor& color) override{}
+                           const RGBAColor& color) override;
     void drawTriangleFan(const std::vector<Vec3>& points, const std::vector<Vec3>& normal,
-                         const RGBAColor& color) override{}
-    void drawFrame(const Vec3& position, const Quaternion& orientation, const Vec3f& size) override{}
+                         const RGBAColor& color) override;
+    void drawFrame(const Vec3& position, const Quaternion& orientation, const Vec3f& size) override;
     void drawFrame(const Vec3& position, const Quaternion& orientation, const Vec3f& size,
                    const RGBAColor& color) override{}
     void drawSpheres(const std::vector<Vec3>& points, const std::vector<float>& radius,
-                     const RGBAColor& color) override{}
+                     const RGBAColor& color) override;
     void drawSpheres(const std::vector<Vec3>& points, float radius,
-                     const RGBAColor& color) override{}
+                     const RGBAColor& color) override;
     void drawFakeSpheres(const std::vector<Vec3>& points, const std::vector<float>& radius,
-                         const RGBAColor& color) override{}
+                         const RGBAColor& color) override;
     void drawFakeSpheres(const std::vector<Vec3>& points, float radius,
-                         const RGBAColor& color) override{}
+                         const RGBAColor& color) override;
     void drawCone(const Vec3& p1, const Vec3& p2, float radius1, float radius2,
-                  const RGBAColor& color, int subd) override{}
-    void drawCube(const float& radius, const RGBAColor& color, const int& subd) override{}
+                  const RGBAColor& color, int subd) override;
+    void drawCube(const float& radius, const RGBAColor& color, const int& subd) override;
     void drawCylinder(const Vec3& p1, const Vec3& p2, float radius, const RGBAColor& color,
-                      int subd) override{}
+                      int subd) override;
     void drawCapsule(const Vec3& p1, const Vec3& p2, float radius, const RGBAColor& color,
-                     int subd) override{}
+                     int subd) override;
     void drawArrow(const Vec3& p1, const Vec3& p2, float radius, const RGBAColor& color,
-                   int subd) override{}
+                   int subd) override;
     void drawArrow(const Vec3& p1, const Vec3& p2, float radius, float coneLength,
-                   const RGBAColor& color, int subd) override{}
+                   const RGBAColor& color, int subd) override;
     void drawArrow(const Vec3& p1, const Vec3& p2, float radius, float coneLength, float coneRadius,
-                   const RGBAColor& color, int subd) override{}
-    void drawCross(const Vec3& p, float length, const RGBAColor& color) override{}
-    void drawPlus(const float& radius, const RGBAColor& color, const int& subd) override{}
-    void drawPoint(const Vec3& p, const RGBAColor& c) override{}
-    void drawPoint(const Vec3& p, const Vec3& n, const RGBAColor& c) override{}
-    void drawTriangle(const Vec3& p1, const Vec3& p2, const Vec3& p3, const Vec3& normal) override{}
+                   const RGBAColor& color, int subd) override;
+    void drawCross(const Vec3& p, float length, const RGBAColor& color) override;
+    void drawPlus(const float& radius, const RGBAColor& color, const int& subd) override;
+    void drawPoint(const Vec3& p, const RGBAColor& c) override;
+    void drawPoint(const Vec3& p, const Vec3& n, const RGBAColor& c) override;
+    void drawTriangle(const Vec3& p1, const Vec3& p2, const Vec3& p3, const Vec3& normal) override;
     void drawTriangle(const Vec3& p1, const Vec3& p2, const Vec3& p3, const Vec3& normal,
-                      const RGBAColor& c) override{}
+                      const RGBAColor& c) override;
     void drawTriangle(const Vec3& p1, const Vec3& p2, const Vec3& p3, const Vec3& normal,
-                      const RGBAColor& c1, const RGBAColor& c2, const RGBAColor& c3) override{}
+                      const RGBAColor& c1, const RGBAColor& c2, const RGBAColor& c3) override;
     void drawTriangle(const Vec3& p1, const Vec3& p2, const Vec3& p3, const Vec3& normal1,
                       const Vec3& normal2, const Vec3& normal3, const RGBAColor& c1,
-                      const RGBAColor& c2, const RGBAColor& c3) override{}
+                      const RGBAColor& c2, const RGBAColor& c3) override;
     void drawQuad(const Vec3& p1, const Vec3& p2, const Vec3& p3, const Vec3& p4,
-                  const Vec3& normal) override{}
+                  const Vec3& normal) override;
     void drawQuad(const Vec3& p1, const Vec3& p2, const Vec3& p3, const Vec3& p4,
-                  const Vec3& normal, const RGBAColor& c) override{}
+                  const Vec3& normal, const RGBAColor& c) override;
     void drawQuad(const Vec3& p1, const Vec3& p2, const Vec3& p3, const Vec3& p4,
                   const Vec3& normal, const RGBAColor& c1, const RGBAColor& c2, const RGBAColor& c3,
-                  const RGBAColor& c4) override{}
+                  const RGBAColor& c4) override;
     void drawQuad(const Vec3& p1, const Vec3& p2, const Vec3& p3, const Vec3& p4,
                   const Vec3& normal1, const Vec3& normal2, const Vec3& normal3,
                   const Vec3& normal4, const RGBAColor& c1, const RGBAColor& c2,
-                  const RGBAColor& c3, const RGBAColor& c4) override{}
-    void drawQuads(const std::vector<Vec3>& points, const RGBAColor& color) override{}
-    void drawQuads(const std::vector<Vec3>& points, const std::vector<RGBAColor>& colors) override{}
+                  const RGBAColor& c3, const RGBAColor& c4) override;
+    void drawQuads(const std::vector<Vec3>& points, const RGBAColor& color) override;
+    void drawQuads(const std::vector<Vec3>& points, const std::vector<RGBAColor>& colors) override;
     void drawTetrahedron(const Vec3& p0, const Vec3& p1, const Vec3& p2, const Vec3& p3,
-                         const RGBAColor& color) override{}
+                         const RGBAColor& color) override;
     void drawScaledTetrahedron(const Vec3& p0, const Vec3& p1, const Vec3& p2, const Vec3& p3,
-                               const RGBAColor& color, const float scale) override{}
-    void drawTetrahedra(const std::vector<Vec3>& points, const RGBAColor& color) override{}
+                               const RGBAColor& color, const float scale) override;
+    void drawTetrahedra(const std::vector<Vec3>& points, const RGBAColor& color) override;
     void drawScaledTetrahedra(const std::vector<Vec3>& points, const RGBAColor& color,
-                              const float scale) override{}
+                              const float scale) override;
     void drawHexahedron(const Vec3& p0, const Vec3& p1, const Vec3& p2, const Vec3& p3,
                         const Vec3& p4, const Vec3& p5, const Vec3& p6, const Vec3& p7,
-                        const RGBAColor& color) override{}
-    void drawHexahedra(const std::vector<Vec3>& points, const RGBAColor& color) override{}
+                        const RGBAColor& color) override;
+    void drawHexahedra(const std::vector<Vec3>& points, const RGBAColor& color) override;
     void drawScaledHexahedra(const std::vector<Vec3>& points, const RGBAColor& color,
-                             const float scale) override{}
-    void drawSphere(const Vec3& p, float radius) override{}
-    void drawSphere(const Vec3& p, float radius, const RGBAColor& color) override{}
-    void drawEllipsoid(const Vec3& p, const Vec3& radii) override{}
-    void drawBoundingBox(const Vec3& min, const Vec3& max, float size) override{}
-    void draw3DText(const Vec3& p, float scale, const RGBAColor& color, const char* text) override{}
+                             const float scale) override;
+    void drawSphere(const Vec3& p, float radius) override;
+    void drawSphere(const Vec3& p, float radius, const RGBAColor& color) override;
+    void drawEllipsoid(const Vec3& p, const Vec3& radii) override;
+    void drawBoundingBox(const Vec3& min, const Vec3& max, float size) override;
+    void draw3DText(const Vec3& p, float scale, const RGBAColor& color, const char* text) override;
     void draw3DText_Indices(const std::vector<Vec3>& positions, float scale,
-                            const RGBAColor& color) override{}
+                            const RGBAColor& color) override;
     void pushMatrix() override{}
     void popMatrix() override{}
     void multMatrix(float*) override{}
